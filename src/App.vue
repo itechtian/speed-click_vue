8452d717b0ba14da2f0cee180760c44dbb88c4d4
Clark <clark@Sinclairs-MacBook-Pro.local>
2022-09-08 10:58:27 +0100

    FIRST STAGE OF THIS APP

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+<!--    <h1>{{ userInfo.name }}</h1>-->
+<!--    <h1>{{ userInfo.age }} years</h1>-->
+<!--    <h1>{{ userInfo.country }}</h1>-->
+<!--    <p>{{homeParagraph}}</p>-->
+   {{count}}
+    <div class="count-div">
+          {{count <= 0 ? count = 0 : count}} km/s
     </div>
-    <router-view/>
+    <div>
+      <button v-on:click="count++" class="btn">{{buttonValue.increment}}</button>
+      <button v-on:click="count--" class="btn">{{buttonValue.decrement}}</button>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data(){
+    return {
+      userInfo:{
+            name:'Clark',
+            age:40,
+            country:'Nigeria'
+        },
+
+        homeParagraph:"this is a paragraph, on my page.",
+         buttonValue:{
+             increment:'Increase',
+              decrement:'Decrease'
+         },
+        count:0
+
+
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.count-div{
+  font-size: 300px !important;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+  border: solid red;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.btn{
+  width: 10%;
+  height: 5vh;
+  margin: 2%;
 }
 </style>
